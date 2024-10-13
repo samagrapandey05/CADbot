a8b82ee63142db8ef5a27a6905c44dfd53b41598
Samagra Pandey <samagrapandey@Samagras-MacBook-Air.local>
2024-10-13 02:58:12 -0700

    Removed Vite Plugin

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import wasm from 'vite-plugin-wasm';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  /*optimizeDeps: {
-    include: ['opencascade.js'],  // Ensure opencascade.js is optimized
-    force: true                   // Force reoptimization
-}*/
 })
