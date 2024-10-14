719a5df223e38b8314d9fed6c96a71e5ae363ad5
Samagra Pandey <samagrapandey@Samagras-MacBook-Air.local>
2024-10-13 21:20:47 -0700

    Homepage CAD Model

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import opencascade from "../node_modules/opencascade.js/dist/opencascade.full.js"
+import opencascadeWasm from "../node_modules/opencascade.js/dist/opencascade.full.wasm?url"
+import { visualizeShapes } from "./visualize.js"; 
+
 function Home(props) {
+    const [modelURL, setModelURL] = useState(null)
+    useEffect(()=>{
+        opencascade({locateFile: () => opencascadeWasm,}).then((oc)=>{
+            const box = new oc.BRepPrimAPI_MakeBox_2(5, 5, 5).Shape();
+            //const torus = new oc.BRepPrimAPI_MakeTorus_1(5, 3).Shape();
+            //const cone = new oc.BRepPrimAPI_MakeCone_1(radius, null, height).Shape();
+            const res = visualizeShapes(oc, box);
+            setModelURL(res);
+            console.log(res)
+        })
+    }, [])
+    const handleMouseMove = (e) => {
+        const mouse = [e.clientX, e.clientY];
+		const modelViewer = document.getElementById("homeViewer")
+		modelViewer.cameraOrbit = `${mouse[0]}deg ` + `${mouse[1] + 45}deg ` + `"110%"`
+    }
     const navigateHome = useNavigate(); 
     const routeSignup = () =>{ 
         const pathCadbot = '/cadbot'; 
         navigateHome(pathCadbot);
     }
     return (
-      /*<div>*/
-      
       <div className="homeBody">
+        <div className="homeCAD">
+            <model-viewer src={modelURL} camera-controls enable-pan id="homeViewer"/>
+        </div>
+        <h1 className="homeBodyTitle">From Thoughts to CAD Files, in Seconds</h1>
+      {/*<div className="homeBody">
         <h1 className="homeBodyTitle">From Thoughts to STL Files, in Seconds</h1>
         {
         (props.loggedIn === false) ? 
@@ -20,8 +44,8 @@ function Home(props) {
                 Generate Files
             </button>
         }
+    </div>*/}
       </div>
-      /*</div>*/
     )
   }
   
